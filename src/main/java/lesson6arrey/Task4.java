fa3384a67e58d06d487f2602fa692766048ed6af
Hennadiy <45887822+geen92@users.noreply.github.com>
2021-07-04 16:37:58 +0300

    Update Task4.java
    fix bug

--- src/main/java/lesson6arrey/Task4.java
+++ src/main/java/lesson6arrey/Task4.java
@@ -13,27 +13,34 @@ import java.util.Arrays;
 public class Task4 {
     public static void main(String[] args) {
         int[] mass = {12, 6, 23, 9, 3, 4, 21};
-        boolean isSorted = false;
-        int buf;
-        while(!isSorted) {
-            isSorted = true;
-            for (int i = 0; i < mass.length-1; i++) {
-                if(mass[i] < mass[i+1]){
-                    isSorted = false;
-                    buf = mass[i];
-                    mass[i] = mass[i+1];
-                    mass[i+1] = buf;
-                }
-            }
+        for (int j = 0; j < mass.length - 1; j++) {
+            for (int i = 0; i < mass.length - 1; i++)
+            if(mass[i] > mass[i + 1]) {
+                int s = mass[i + 1];
+                mass[i + 1] = mass[i];
+                mass[i] = s;
         }
-        System.out.println("Сортировка по убыванию цикл `for`: ");
+//        boolean isSorted = false;
+//        int buf;
+//        while(!isSorted) {
+//            isSorted = true;
+//            for (int i = 0; i < mass.length-1; i++) {
+//                if(mass[i] < mass[i+1]){
+//                    isSorted = false;
+//                    buf = mass[i];
+//                    mass[i] = mass[i+1];
+//                    mass[i+1] = buf;
+//                }
+//            }
+        }
+        System.out.println("Сортировка по убыванию цикл for: ");
         for (int i = 0; i < mass.length; i++) {
             System.out.println(mass[i]);
     }
-        System.out.println("Сортировка по убыванию цикл `for each`: ");
+        System.out.println("Сортировка по убыванию цикл for each: ");
         for (int i:mass) {
             System.out.println(i);
     }
-        System.out.println("Сортировка по убыванию цикл `Arrays.toString`: " + Arrays.toString(mass));
+        System.out.println("Сортировка по убыванию цикл Arrays.toString: " + Arrays.toString(mass));
     }
 }
