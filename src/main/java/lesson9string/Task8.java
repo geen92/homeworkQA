02f9f581e0d4eb11f69f935cd2038b3f21e57ffb
geen92 <ponogeen@gmail.com>
2021-07-07 17:51:45 +0300

    Добавил задачи 7 8 9

---
+++ src/main/java/lesson9string/Task8.java
@@ -0,0 +1,23 @@
+//8) Изменить регулярное выражение в примере – пусть reference1.org
+//тоже считается ссылкой.
+//
+//  import java.util.regex.Matcher;
+//  import java.util.regex.Pattern;
+//  public class RegexDemo3 {
+//      public static void main(String[] args) {
+//          System.out.println(test("google.com"));
+//          System.out.println(test("reference1.ua"));
+//          System.out.println(test("reference1.org"));
+//}
+//      public static boolean test(String testString) {
+//          Pattern pattern =
+//  Pattern.compile(".+\\.(com|ua|ru)");
+//          Matcher matcher = pattern.matcher(testString);
+//          return matcher.matches();
+//}
+//}
+
+package lesson9string;
+
+public class Task8 {
+}
---
+++ src/main/java/lesson9string/Task9.java
@@ -0,0 +1,8 @@
+//9) Дана строка “Versions: Java  5, Java 6, Java 7, Java 8, Java 12.”
+//Найти все подстроки "Java X" и распечатать их. Использовать
+//регулярные выражения
+
+package lesson9string;
+
+public class Task9 {
+}
