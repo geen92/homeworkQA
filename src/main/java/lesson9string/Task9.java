9854de5d3c7472a7c68888824d563fc59617c6e2
geen92 <45887822+geen92@users.noreply.github.com>
2021-07-07 23:37:38 +0300

    Сделал задачю 9

--- src/main/java/lesson9string/Task9.java
+++ src/main/java/lesson9string/Task9.java
@@ -3,6 +3,17 @@
 //регулярные выражения
 
 package lesson9string;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Task9 {
+    public static void main(String[] args) {
+        String regularExpression = "Java\\s+\\d{1,2}";
+        String inputString = "Versions: Java  5, Java 6, Java 7, Java 8, Java 12.";
+        Pattern pattern = Pattern.compile(regularExpression);
+        Matcher match = pattern.matcher(inputString);
+        while (match.find()){
+            System.out.println(match.group());
+        }
+    }
 }
