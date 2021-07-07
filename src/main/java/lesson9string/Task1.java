50aa064750b942e56930e88916c48bd9eedc38a7
geen92 <ponogeen@gmail.com>
2021-07-07 14:49:44 +0300

    fix task 1 and task 3

--- src/main/java/lesson9string/Task1.java
+++ src/main/java/lesson9string/Task1.java
@@ -17,32 +17,3 @@ public class Task1 {
         Task1.lineOutput("Пономарев", 4, "Географии");
     }
 }
-
-//    public static void main(String[] args) {
-//    }
-//        String surname = "2";
-//        String rating = "2";
-//        String subject = "3";
-//        System.out.println("Студент" + surname + "получил" + rating + "по" + subject);
-//    }
-//    public static void lineOutput (String [] );
-//        System.out.println("Студент" + surname + "получил" + rating + "по" + subject);
-//
-//    }
-//}
-//
-//        String surname = "1";
-//        int rating = 5;
-//        String subject = "2";
-//        String result = String.format("Студент %s получил %d по %s",surname,rating,subject);
-//        System.out.println(result);
-//    }
-//    static class Methods {
-//        public static void div(int a, int b) {
-//            if (( a == 0 ) || ( b == 0  )) {
-//                System.out.println("You cannot divide by zero");
-//            } else {
-//                System.out.println(a / b);
-//            }
-//        }
-//}}
--- src/main/java/lesson9string/Task3.java
+++ src/main/java/lesson9string/Task3.java
@@ -15,19 +15,25 @@ public class Task3 {
         String s2 = "6 minus 5";
         String s3 = "8 plus 10";
         String s4 = "16 minus 5";
-
-        String[] expression = s2.split("\\s");
+        Task3.expressionMath(s1);
+        Task3.expressionMath(s2);
+        Task3.expressionMath(s3);
+        Task3.expressionMath(s4);
+    }
+    public static void expressionMath(String x) {
+        String[] expression = x.split("\\s");
         int temp = 0;
         if (expression[1].contains("plus"))
         {
             temp = (Integer.parseInt(expression[0]) + Integer.parseInt(expression[2]));
-            System.out.println(s2 + " = " + temp);
+            System.out.println(x + " = " + temp);
         }
         else if ((expression[1].contains("minus")))
         {
             temp = (Integer.parseInt(expression[0]) - Integer.parseInt(expression[2]));
-            System.out.println(s2 + " = " + temp);
+            System.out.println(x + " = " + temp);
         }
         else System.out.println("Некорректное выражение!");
     }
 }
+
