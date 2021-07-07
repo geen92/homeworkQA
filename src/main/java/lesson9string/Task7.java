17ea26f4935f46980b88f455eceb408c305ddca5
geen92 <45887822+geen92@users.noreply.github.com>
2021-07-07 23:05:04 +0300

    Сделал задачю 7

--- src/main/java/lesson9string/Task7.java
+++ src/main/java/lesson9string/Task7.java
@@ -10,4 +10,34 @@
 package lesson9string;
 
 public class Task7 {
+    public static void main(String[] args) {
+
+        StringBuffer Buffer1 = new StringBuffer("3");
+            Buffer1.append(" + ");
+            Buffer1.append("56");
+            Buffer1.append(" = ");
+            Buffer1.append("59");
+        System.out.println(Buffer1);
+
+        StringBuffer Buffer2 = new StringBuffer("3");
+            Buffer2.append(" - ");
+            Buffer2.append("56");
+            Buffer2.append(" = ");
+            Buffer2.append("-53");
+        System.out.println(Buffer2);
+
+        StringBuffer Buffer3 = new StringBuffer("3");
+            Buffer3.append(" * ");
+            Buffer3.append("56");
+            Buffer3.append(" = ");
+            Buffer3.append("168");
+        System.out.println(Buffer3);
+
+        Buffer1.replace(7, 8, "равно");
+        System.out.println(Buffer1);
+        Buffer2.replace(7, 8, "равно");
+        System.out.println(Buffer2);
+        Buffer3.replace(7, 8, "равно");
+        System.out.println(Buffer3);
+    }
 }
