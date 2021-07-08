02731d14ee1f1f2d248f240fd29a8a464f910806
geen92 <ponogeen@gmail.com>
2021-07-08 09:08:41 +0300

    fix задачи 7  9

--- src/main/java/lesson9string/Task7.java
+++ src/main/java/lesson9string/Task7.java
@@ -41,3 +41,23 @@ public class Task7 {
         System.out.println(Buffer3);
     }
 }
+
+//public class Task7 {
+//
+//    public static void main(String[] args) {
+//
+//        StringBuilder stringBuilder = createSomeString(3, 56);
+//        System.out.println(stringBuilder);
+//
+//    }
+//
+//    private static StringBuilder createSomeString(int a, int b) {
+//
+//        StringBuilder result = new StringBuilder();
+//        result.append(a).append(" + ").append(b).append(" = ").append(a + b).append("\n")
+//                .append(a).append(" - ").append(b).append(" = ").append(a - b).append("\n")
+//                .append(a).append(" * ").append(b).append(" = ").append(a * b).append("\n");
+//        return result;
+//    }
+//
+//}
--- src/main/java/lesson9string/Task9.java
+++ src/main/java/lesson9string/Task9.java
@@ -17,3 +17,19 @@ public class Task9 {
         }
     }
 }
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+//
+//public class Task9 {
+//    public static void main(String[] args) {
+//
+//        String string = "Versions: Java 5, Java 6, Java 7, Java 8, Java 12.";
+//        String patternString = "Java\\s+\\d{1,2}";
+//        Pattern pattern = Pattern.compile(patternString);
+//        Matcher matcher = pattern.matcher(string);
+//        while(matcher.find()){
+//            System.out.println(matcher.group());
+//        }
+//    }
+//
+//}
